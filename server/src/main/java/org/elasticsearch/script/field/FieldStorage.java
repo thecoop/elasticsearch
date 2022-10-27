bd421eeef89a8d810de8ac8e1371bab836409f39
Simon Cooper <simon.cooper@elastic.co>
2022-10-27 17:05:31 +0100

    Some test cases

--- server/src/main/java/org/elasticsearch/script/field/FieldStorage.java
+++ server/src/main/java/org/elasticsearch/script/field/FieldStorage.java
@@ -109,7 +109,6 @@ public class FieldStorage {
         }
 
         Object setValue(Object value) {
-            assert value instanceof Node == false;
             Object existing = this.value;
             this.value = value;
             return existing;
@@ -351,7 +350,6 @@ public class FieldStorage {
 
                                 @Override
                                 public Object setValue(Object value) {
-                                    // TODO(stu): handle NestedCtxMap
                                     return entry.getValue().ctxSet(value);
                                 }
                             };
@@ -360,7 +358,6 @@ public class FieldStorage {
                         @Override
                         public void remove() {
                             // TODO(stu): implement
-                            // need to have access to full trail back to root, so can delete nested containers as we go
                         }
                     };
                 }
