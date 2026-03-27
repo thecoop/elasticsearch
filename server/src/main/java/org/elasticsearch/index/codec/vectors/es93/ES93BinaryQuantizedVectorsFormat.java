65ad9d0a2ff436b54f6b24ba02435c2e6d06730e
Simon Cooper <simon.cooper@elastic.co>
2026-03-27 09:30:04 +0000

    Use a flat delegate that works with bfloat16

--- server/src/main/java/org/elasticsearch/index/codec/vectors/es93/ES93BinaryQuantizedVectorsFormat.java
+++ server/src/main/java/org/elasticsearch/index/codec/vectors/es93/ES93BinaryQuantizedVectorsFormat.java
@@ -19,7 +19,7 @@
  */
 package org.elasticsearch.index.codec.vectors.es93;
 
-import org.apache.lucene.codecs.hnsw.FlatVectorScorerUtil;
+import org.apache.lucene.codecs.hnsw.DefaultFlatVectorScorer;
 import org.apache.lucene.codecs.hnsw.FlatVectorsReader;
 import org.apache.lucene.codecs.hnsw.FlatVectorsScorer;
 import org.apache.lucene.codecs.hnsw.FlatVectorsWriter;
@@ -91,9 +91,10 @@ public class ES93BinaryQuantizedVectorsFormat extends AbstractFlatVectorsFormat 
 
     public static final String NAME = "ES93BinaryQuantizedVectorsFormat";
 
-    private static final ES818BinaryFlatVectorsScorer scorer = new ES818BinaryFlatVectorsScorer(
-        FlatVectorScorerUtil.getLucene99FlatVectorsScorer()
-    );
+    // whilst the binary scorer should never get into a situation where it's trying to score
+    // raw values, it's still a good idea to use a fallback scorer that supports
+    // all the flat vector formats being used here, including bfloat16
+    private static final ES818BinaryFlatVectorsScorer scorer = new ES818BinaryFlatVectorsScorer(DefaultFlatVectorScorer.INSTANCE);
 
     private final ES93GenericFlatVectorsFormat rawFormat;
 
